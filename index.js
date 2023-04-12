bd2036556e30d589325f6207dbe76cbd6fdbcef4
Buntin-Synthia <aokitakumi72@gmail.com>
2023-04-13 06:21:14 +0900

    yes

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ require('dotenv').config();
 console.log(process.env.token);
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ],
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
 });
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('messageCreate', (message) => {
-  if (message.content.includes('まさ')) {
-    message.reply('https://raw.buntin.xyz/masaiei.png');
-  }
+    if (message.content.includes('まさ')) {
+        message.reply('https://raw.buntin.xyz/masaiei.png');
+    }
 });
 
 client.login(process.env.token);
