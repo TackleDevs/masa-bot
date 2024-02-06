c1ba3e68d5a71d2aac900a8499e5d47f16de9cbd
Buntin-Synthia <mail@buntin.xyz>
2024-02-06 21:33:52 +0900

    松永大降臨

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log("Hello via Bun!");
+import { Client, GatewayIntentBits, Events } from 'discord.js';
+
+const words: string[] = ['まさ', 'まつなが', 'マサ', 'マツナガ', '大', '松永', 'masa', 'matsunaga', 'matunaga'];
+
+if (Bun.env.TOKEN === undefined) {
+  console.error('TOKEN is not set!!');
+  process.exit(1);
+}
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+});
+
+client.on(Events.ClientReady, () => {
+  const user = client.user || { tag: 'unknown' };
+  console.log(`Logged in as ${user.tag}!`);
+});
+
+client.on(Events.MessageCreate, (message) => {
+  if(message.author.bot) {
+    return;
+  }
+  const replyMessage: string = 'https://raw.buntin.xyz/masaiei.png';
+  if (words.some((word) => message.content.includes(word))) {
+    message.reply(replyMessage);
+  }
+});
+
+client.login(Bun.env.TOKEN);
