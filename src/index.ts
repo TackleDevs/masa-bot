4898941dd51c2c86b4975dc3edb8f605bbfd9716
synthia.liz <mail@buntin.xyz>
2024-03-27 18:19:06 +0900

    init elysia

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,7 @@
-import { Client, GatewayIntentBits, Events } from 'discord.js';
+import { Elysia } from "elysia";
 
-const words: string[] = ['まさ', 'まつなが', 'マサ', 'マツナガ', '大', '松永', 'masa', 'matsunaga', 'matunaga'];
+const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
 
-if (Bun.env.TOKEN === undefined) {
-  console.error('TOKEN is not set!!');
-  process.exit(1);
-}
-
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds, 
-    GatewayIntentBits.GuildMessages, 
-    GatewayIntentBits.MessageContent
-  ],
-});
-
-client.on(Events.ClientReady, () => {
-  const user = client.user || { tag: 'unknown' };
-  console.log(`Logged in as ${user.tag}!`);
-});
-
-client.on(Events.MessageCreate, (message) => {
-  if (message.author.bot) {
-    return;
-  }
-  const replyMessage: string = 'https://raw.buntin.xyz/masaiei.png';
-  if (words.some((word) => message.content.includes(word))) {
-    message.reply(replyMessage);
-  }
-});
-
-client.login(Bun.env.TOKEN);
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
